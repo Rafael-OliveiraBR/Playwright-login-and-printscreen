c70faffbc648c5cdaec33f89b281c2d9f2e379b3
Rafael <rsdo@outlook.com>
2022-01-06 22:27:48 -0300

    update header

---
+++ account.py
@@ -0,0 +1,3 @@
+
+user = 'xxxx@xxxxxxxx.xxx'
+password = 'xxxxxxxxxxx'
--- main.py
+++ main.py
@@ -1,8 +1,25 @@
+###########################################################
+"""
+    esse é um simples script para automatizar o login
+    de um site, acessar uma página e fazer um print
+    da tela do navegador, de forma automatizada.
+    o mesmo exemplo pode ser usado para testes ou
+    coletar informações de sites/páginas.
+
+    AUTOR: Rafael Santos de Oliveira
+    github: https://github.com/Rafael-OliveiraBR
+    linkedin: https://www.linkedin.com/in/rafael-oliveira-16366389/
+
+"""
+###########################################################
+
+# imports
 from playwright.sync_api import sync_playwright
+from account import *
 
 
 ###########################################################
-#   SETUP BROWSER
+#   SETUP DO BROWSER, EXECUÇÃO DAS AUTOMAÇÕES
 ###########################################################
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=False, slow_mo=100)
@@ -12,12 +29,12 @@ with sync_playwright() as p:
 
 
     # --------------------------
-    # EXECUTE ACTIONS
+    # EXECUTA AÇÕES
     # --------------------------
     """ Faz login, acessa perfil, tira print e desloga da conta """
     page.click("text=Sign in")
-    page.fill('input[name="login"]', 'rsdo@outlook.com')
-    page.fill('input[name="password"]', 'redbanger33')
+    page.fill('input[name="login"]', user)
+    page.fill('input[name="password"]', password)
     page.click('input[name="commit"]')
     #page.wait_for_timeout(1000)
     page.click('[aria-label="View profile and more"]')
@@ -32,9 +49,9 @@ with sync_playwright() as p:
     page.click('[class="dropdown-item dropdown-signout"]')
     #page.wait_for_timeout(4000)
 
-    # Save storage state into the file.
+    # Salva dados armazenados em um arquivo
     storage = context.storage_state(path="state.json")
 
-    # Create a new context with the saved storage state.
+    # Cria um novo context com os dados salvos.
     context = browser.new_context(storage_state="state.json")
 
